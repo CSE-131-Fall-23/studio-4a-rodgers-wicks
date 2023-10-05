8c0c76cf4e32c08e1e7ccd5012bdbf6fdbe8a4ec
alaynarodgers <alaynarodgers@localhost>
2023-10-05 12:48:49 -0500

    finished studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,16 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.clear();
+		StdDraw.setPenColor(252, 202, 75);
+		StdDraw.filledRectangle(0.5, 0.5, 0.7, 0.4);
+		StdDraw.setPenColor(208, 160, 50);
+		StdDraw.filledCircle(0.5, 0.5, 0.05);
+		StdDraw.filledCircle(0.25, 0.37, 0.07);
+		StdDraw.filledCircle(0.89, 0.67, 0.05);
+		StdDraw.filledCircle(0.43, 0.28, 0.03);
+		StdDraw.filledCircle(0.62, 0.73, 0.03);
+		StdDraw.filledCircle(0.34, 0.20, 0.05);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,37 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		int R = in.nextInt();
+		int G = in.nextInt();
+		int B = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+		
+		if(shapeType.equals("rectangle")) {
+			Color outline = new Color(R, G, B);
+			StdDraw.setPenColor(outline);
+			if(isFilled == true) {
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			else
+			StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+		
+		}
+		
+		
+		
+		
+		
+			
+		}
+
+	private static void rectangle(double parameterOne, double parameterTwo, double parameterThree,
+			double parameterFour) {
+		// TODO Auto-generated method stub
+		
 	}
-}
+	}
